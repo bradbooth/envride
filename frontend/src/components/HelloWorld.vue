0975fb1616dd552ae9b17ba784d071e1de4fc836
Brad <brad_booth@live.com>
2020-01-14 19:25:17 -0500

    Basic vehicle data fetching

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+
+    <VehicleSelect />
+
+  </div>
+</template>
+
+<script>
+
+import VehicleSelect from './vehicle/VehicleSelect'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    VehicleSelect
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
